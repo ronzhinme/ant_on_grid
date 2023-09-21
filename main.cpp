11557b2ac211ee4cf8d8f1e45cb78cb00e84a13f
Mikhail Ronzhin <ronme@mail.ru>
2023-09-21 19:33:16 +0600

    task solved

--- main.cpp
+++ main.cpp
@@ -22,11 +22,73 @@
 // https://github.com/Boring-Mind/Ant-assignment/issues/1
 
 #include <iostream>
+#include <sstream>
+#include <stack>
+#include <set>
 
 using namespace std;
 
+using Point = std::pair<int64_t, int64_t>;
+std::set<Point> processed_point;
+std::stack<Point> points_to_process;
+
+static bool point_unavailable(const Point &point)
+{
+    static const int Max_digit_summ = 25;
+    std::stringstream sstream;
+    sstream << point.first << point.second;
+    auto summ = 0;
+    while(const char ch = sstream.get())
+    {
+        if(ch < '0')
+            break;
+
+        summ += ch - '0';
+    }
+
+    return summ > Max_digit_summ;
+}
+
+void process_next_point(const Point& point)
+{
+    // точка уже была обработана, а соседние были добавлены на обработку.
+    if(processed_point.find(point) != processed_point.end())
+    {
+        return;
+    }
+
+    // точка недоступна.
+    if(point_unavailable(point))
+    {
+        return;
+    }
+
+    // добавить соседние 4 точки для обработки.
+    points_to_process.push({point.first, point.second - 1});
+    points_to_process.push({point.first, point.second + 1});
+    points_to_process.push({point.first - 1, point.second});
+    points_to_process.push({point.first + 1, point.second});
+
+    // текущая точка обработана
+    processed_point.insert(point);
+}
+
+int64_t calculate_available_points(const Point& point)
+{
+    process_next_point(point);
+    while(!points_to_process.empty())
+    {
+        const auto point = points_to_process.top();
+        points_to_process.pop();
+        process_next_point(point);
+
+    }
+
+    return processed_point.size();
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::cout << "Max point count: " << calculate_available_points({1000, 1000}) << std::endl;
     return 0;
 }
